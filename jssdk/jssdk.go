1d55c993399718773de02038773a5ca3f3d3ce41
comeonjy <1126254578@qq.com>
2019-10-21 21:47:53 +0800

    jssdk

---
+++ jssdk/jssdk.go
@@ -0,0 +1,194 @@
+package jssdk
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+const (
+	ACCESS_TOKEN_URL = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s"
+	JSAPI_TICKET_URL = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?type=jsapi&access_token=%s"
+)
+
+type Jssdk struct {
+	accessToken      *accessToken
+	jsspiTicket      *jsapiTicket
+	appId, appSecret string
+	storage          Storage
+}
+
+type accessToken struct {
+	Access_token string `json:"access_token"`
+	Expires_in   int64  `json:"expires_in"`
+	Expire_time  int64  `json:"expire_time"`
+}
+
+type jsapiTicket struct {
+	Jsapi_ticket string `json:"ticket"`
+	Expires_in   int64  `json:"expires_in"`
+	Expire_time  int64  `json:"expire_time"`
+}
+
+type Storage interface {
+	saveAccessToken([]byte) error
+	saveJsapiTicket([]byte) error
+	getJsapiTicket() (string, error)
+	getAccessToken() (string, error)
+}
+
+func NewJssdk(appId, appSecret string, s Storage) *Jssdk {
+	if s == nil {
+		s = &_storage{
+			accessTokenPath: "access_token.json",
+			jsapiTicketPath: "jsapi_ticket.json",
+		}
+	}
+	w := &Jssdk{
+		accessToken: &accessToken{},
+		jsspiTicket: &jsapiTicket{},
+		appId:       appId,
+		appSecret:   appSecret,
+		storage:     s,
+	}
+	return w
+
+}
+
+// 获取AccessToken
+// 通过accessToken!=nil来判断是否存在token
+// error仅用作错误信息返回
+func (w *Jssdk) GetAccessToken() (*accessToken, error) {
+
+	// 本地读取AccessToken
+	s, e := w.storage.getAccessToken()
+	if e != nil || s == "" {
+		return nil, e
+	}
+	if err := json.Unmarshal([]byte(s), w.accessToken); err != nil {
+		return nil, err
+	}
+
+	// AccessToken过期判断
+	if w.accessToken.Expire_time > time.Now().Unix() {
+		return w.accessToken, nil
+	}
+
+	// 向微信服务器请求AccessToken
+	url := fmt.Sprintf(ACCESS_TOKEN_URL, w.appId, w.appSecret)
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	bytes, err := ioutil.ReadAll(resp.Body)
+	err = json.Unmarshal(bytes, w.accessToken)
+	if err != nil || w.accessToken == nil {
+		return nil, err
+	}
+
+	// 修改AccessToken过期时间
+	w.accessToken.Expire_time = time.Now().Unix() + 7000
+	tokenStr, err := json.Marshal(w.accessToken)
+	if err != nil {
+		return w.accessToken, err
+	}
+
+	// 存储AccessToken
+	if err := w.storage.saveAccessToken(tokenStr); err != nil {
+		return w.accessToken, err
+	}
+
+	return w.accessToken, nil
+
+}
+
+func (w *Jssdk) GetJsapiTicket() (*jsapiTicket, error) {
+
+	// 本地读取JsapiTicket
+	s, e := w.storage.getJsapiTicket()
+	if e != nil || s == "" {
+		return nil, e
+	}
+	if err := json.Unmarshal([]byte(s), w.jsspiTicket); err != nil {
+		return nil, err
+	}
+
+	// JsapiTicket过期判断
+	if w.jsspiTicket.Expire_time > time.Now().Unix() {
+		return w.jsspiTicket, nil
+	}
+
+	// 获取AccessToken
+	if _, e := w.GetAccessToken(); e!=nil && w.accessToken==nil{
+		return nil,e
+	}
+
+	// 向微信服务器请求JsapiTicket
+	url := fmt.Sprintf(JSAPI_TICKET_URL, w.accessToken.Access_token)
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	bytes, err := ioutil.ReadAll(resp.Body)
+	//{"errcode":0,"errmsg":"ok","ticket":"LIKLckvwlJT9cWIhEQTwfK7_uwZRuHdb8RaqHNtL2IvHpM6A4bNkahw9Q_-XTzq-gBaPRa2xPEwc7D-SVqPARg","expires_in":7200}
+	err = json.Unmarshal(bytes, w.jsspiTicket)
+	if err != nil || w.jsspiTicket == nil {
+		return nil, err
+	}
+
+	// 修改JsapiTicket过期时间
+	w.jsspiTicket.Expire_time = time.Now().Unix() + 7000
+	tokenStr, err := json.Marshal(w.jsspiTicket)
+	if err != nil {
+		return w.jsspiTicket, err
+	}
+
+	// 存储JsapiTicket
+	if err := w.storage.saveJsapiTicket(tokenStr); err != nil {
+		return w.jsspiTicket, err
+	}
+
+	return w.jsspiTicket, nil
+
+}
+
+// 默认存储类型
+// 存储格式：json
+// 存储位置：当前目录
+type _storage struct {
+	accessTokenPath string
+	jsapiTicketPath string
+}
+
+func (s *_storage) saveAccessToken(bytes []byte) error {
+	return s.save(s.accessTokenPath, bytes)
+}
+
+func (s *_storage) getAccessToken() (string, error) {
+	bytes, e := s.read(s.accessTokenPath)
+	return string(bytes), e
+}
+
+func (s *_storage) saveJsapiTicket(bytes []byte) error {
+	return s.save(s.jsapiTicketPath, bytes)
+}
+
+func (s *_storage) getJsapiTicket() (string, error) {
+	bytes, e := s.read(s.jsapiTicketPath)
+	return string(bytes), e
+}
+
+func (s *_storage) read(filename string) ([]byte, error) {
+	_, e := os.Stat(filename)
+	if e != nil {
+		s.save(filename, []byte("{}"))
+	}
+	return ioutil.ReadFile(filename)
+}
+
+func (s *_storage) save(filename string, bytes []byte) error {
+	return ioutil.WriteFile(filename, bytes, os.ModePerm)
+}
---
+++ src/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"wechat-sdk/jssdk"
+)
+
+func main()  {
+	jssdk1 := jssdk.NewJssdk("wx5d648fda319bed0c", "643a09b180876a550e20cea6d32c8095",nil)
+	token, e := jssdk1.GetAccessToken()
+	fmt.Println(token, e)
+	token2, e2 := jssdk1.GetJsapiTicket()
+	fmt.Println(token2, e2)
+}
